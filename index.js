bf332e0662f7bd3df885052acfe207b464a25006
nceeoh7 <51413645+nceeoh7@users.noreply.github.com>
2022-09-28 10:16:36 -0400

    File update

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+
+const app = new express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, "pages/about.html"));
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
+});
+
+app.get("/post", (req, res) => {
+  //res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  res.render("post");
+});
+
+app.listen(port, () => {
+  console.log("App Listening on Port " + port);
+});
